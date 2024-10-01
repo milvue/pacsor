23b42254ac0c70268b6f0423ce48235e6231b4ad
Wilfried Grousson <wilfried@milvue.com>
2024-10-01 11:08:26 +0200

    Version 1.5

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@ source ./scripts/menus.deploy.sh
 source ./scripts/dialogs.deploy.sh
 source ./scripts/validators.deploy.sh
 
-SCRIPT_VERSION="1.4"
+SCRIPT_VERSION="1.5"
 MAIN_TITLE="Pacsor Deployment Configuration Tool v$SCRIPT_VERSION"
 
 # Default values
